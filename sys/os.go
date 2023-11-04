445644d11d9c96bbb3f6bb089c64cc87356fccc4
lollipopkit <lollipopkit@icloud.com>
2023-11-04 15:38:34 +0800

    new: `ExecuteConfig`

--- sys/os.go
+++ sys/os.go
@@ -1,9 +1,29 @@
 package sys
 
 import (
+	"bytes"
 	"os"
 	"os/exec"
 )
+
+type ExecuteConfig struct {
+	Exec string
+	Args []string
+	Dir string
+}
+
+func (ec *ExecuteConfig) Run() (stdout, stderr string, err error) {
+	cmd := exec.Command(ec.Exec)
+	cmd.Dir = ec.Dir
+	var stdout_, stderr_ bytes.Buffer
+    cmd.Stdout = &stdout_  // 标准输出
+    cmd.Stderr = &stderr_  // 标准错误
+	err = cmd.Run()
+
+	stdout = stdout_.String()
+	stderr = stderr_.String()
+	return
+}
 
 func Execute(bin string, args ...string) (string, error) {
 	cmd := exec.Command(bin, args...)
