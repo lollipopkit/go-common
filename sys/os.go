7571347d6e6a7ef9a407dc137467e953b582b211
lollipopkit <lollipopkit@icloud.com>
2023-11-04 20:18:32 +0800

    new: `sys.RunScript`

--- sys/os.go
+++ sys/os.go
@@ -9,15 +9,15 @@ import (
 type ExecuteConfig struct {
 	Exec string
 	Args []string
-	Dir string
+	Dir  string
 }
 
 func (ec *ExecuteConfig) Run() (stdout, stderr string, err error) {
 	cmd := exec.Command(ec.Exec)
 	cmd.Dir = ec.Dir
 	var stdout_, stderr_ bytes.Buffer
-    cmd.Stdout = &stdout_  // 标准输出
-    cmd.Stderr = &stderr_  // 标准错误
+	cmd.Stdout = &stdout_ // 标准输出
+	cmd.Stderr = &stderr_ // 标准错误
 	err = cmd.Run()
 
 	stdout = stdout_.String()
@@ -34,4 +34,21 @@ func Execute(bin string, args ...string) (string, error) {
 func Exist(path string) bool {
 	_, err := os.Stat(path)
 	return err == nil || os.IsExist(err)
+}
+
+// Return stdout, stderr, error
+func RunScript(scriptPath, workDir string, args ...string) (string, string, error) {
+	cat := exec.Command("cat", scriptPath)
+	cat.Dir = workDir
+
+	args = append([]string{"-s", "--"}, args...)
+	run := exec.Command("bash", args...)
+	run.Stdin, _ = cat.StdoutPipe()
+
+	var stdout, stderr bytes.Buffer
+	run.Stdout = &stdout
+	run.Stderr = &stderr
+
+	err := run.Run()
+	return stdout.String(), stderr.String(), err
 }
